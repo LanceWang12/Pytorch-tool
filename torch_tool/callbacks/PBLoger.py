f87d7689f1d2de35460b50bef38c2d4f187c2f56
Lance Wang <58240390+LanceWang12@users.noreply.github.com>
2021-05-21 02:23:00 +0800

    Update torch_tool
    split the trainer to KerasModule and TorchModule

---
+++ torch_tool/callbacks/PBLoger.py
@@ -0,0 +1,47 @@
+from .Callback import Callback
+
+class PBLoger(Callback):
+    def __init__(
+        self,
+        callbacks=None,
+        add_history=False,
+        add_progbar=False,
+        model=None,
+        **params
+    ):
+        pass
+    
+    def on_train_begin(self):
+        # progress bar's length, initialization
+        # history and ?log?
+        raise NotImplementedError('This is virtual member function. Please overwrite!')
+
+    def on_train_end(self):
+        raise NotImplementedError('This is virtual member function. Please overwrite!')
+
+    def on_epoch_begin(self):
+        # print epoch
+        # reset log
+        raise NotImplementedError('This is virtual member function. Please overwrite!')
+
+    def on_epoch_end(self):
+        # update PB
+        # record history
+        # callback
+        raise NotImplementedError('This is virtual member function. Please overwrite!')
+
+    def on_batch_begin(self):
+        # update loss of log
+        raise NotImplementedError('This is virtual member function. Please overwrite!')
+
+    def on_batch_end(self):
+        # print message 
+        raise NotImplementedError('This is virtual member function. Please overwrite!')
+
+    def on_test_begin(self):
+        # update log of training acc, validation loss acc
+        raise NotImplementedError('This is virtual member function. Please overwrite!')
+
+    def on_test_end(self):
+        raise NotImplementedError('This is virtual member function. Please overwrite!')
+
